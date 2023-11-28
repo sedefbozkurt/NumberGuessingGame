5351c0325dc0d3b141380b0266c0ff4630d53c63
Sedef Bozkurt <sedefbozkurt@Sedefs-Mac.local>
2023-11-28 10:47:52 +0300

    Farklı zorluk seviyeleri eklendi.

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -1,12 +1,31 @@
 import random
 
 def numberGuessingGame():
-    number = random.randint(1, 100)
-    attempts = 0
-    maxAttemps = 5
-
     print("Sayı tahmin etme oyununa hoş geldiniz!")
-    print("1 ile 100 arasında bir sayı tuttum bakalım onu bulabilecek misin?")
+    print("Zorluk seviyesini seçin: ")
+    print("1. Kolay (1-50)")
+    print("2. Orta (1-100)")
+    print("3. Zor (1-200)")
+
+    while True:
+        choice = input("Seçiminizi giriniz (1-3): ")
+
+        if choice == '1':
+            number = random.randint(1, 50)
+            maxAttempts = 7
+        elif choice == '2':
+            number = random.randint(1, 100)
+            maxAttemps = 5
+        elif choice == '3':
+            number = random.randint(1, 200)
+            maxAttempts = 3
+            break
+        else:
+            print("Geçersiz seçim. Lütfen 1-3 aralığında bir seçim yapınız!")
+    attempts = 0
+
+    print(f"1 ile {number} arasında bir sayı tuttum bakalım onu bulabilecek misin?"
+          f"Dikkatli ol {maxAttemps} deneme hakkın var.")
 
     while attempts < maxAttemps:
         guess = int(input("Tahmininizi giriniz: "))
